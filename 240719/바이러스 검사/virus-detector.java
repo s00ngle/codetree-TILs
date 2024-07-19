e4b65de389241ecd3df29cd9ab82aac6d135a179
Soongle <42750885+s00ngle@users.noreply.github.com>
2024-07-19 13:52:03 +0900

    Title: 바이러스 검사, Time: 509ms, Memory: 68MB, Status: Wrong Answer - CodeTree

---
+++ 240719/바이러스 검사/virus-detector.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int[] customers = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            customers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int leader = Integer.parseInt(st.nextToken());
+        int crew = Integer.parseInt(st.nextToken());
+
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            customers[i] -= leader;
+            result++;
+            if (customers[i] > 0) {
+                result += customers[i] / crew;
+                if (customers[i] % crew != 0) {
+                    result++;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
